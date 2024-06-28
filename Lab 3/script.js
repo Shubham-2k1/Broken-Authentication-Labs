28d7c0fc97cd4ab5a7bb3d435d1564d32e67404b
Shubham <vishu.bhardwaz2001@gmail.com>
2024-06-28 18:22:20 +0530

    Labs published

---
+++ Lab 3/script.js
@@ -0,0 +1,31 @@
+document.getElementById('login-form').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+
+    let response = await fetch('https://auth3api.onrender.com/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: `username=${username}&password=${password}`
+    });
+
+    let result = await response.json();
+    
+    if (response.ok) {
+        document.getElementById('message').textContent = 'Login successful!';
+    } else {
+        if (result.error === 'Invalid password') {
+            document.getElementById('message').textContent = 'Invalid Credentials. Please try again.';
+        } else if (result.error === 'Invalid username') {
+            document.getElementById('message').textContent = 'Invalid Credentials. Please try again.';
+        } else if(result.error === 'Too Many request from this user, Try after 1 minute'){
+            document.getElementById('message').textContent = 'Too Many Request from this user, Try after 1 minute';
+        }
+        else {
+            document.getElementById('message').textContent = 'Failed to authenticate. Please try again later.';
+        }
+    }
+});
